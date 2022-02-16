65c6efe8dbffbabe45bb144ca197997e72f6de80
Jesse Friberg <jesse.friberg@edu.turkuamk.fi>
2022-02-16 17:17:33 +0200

    Added comments

--- rename_pictures.py
+++ rename_pictures.py
@@ -1,11 +1,15 @@
 import os
 
+
+#Path of the folder you want to rename the insides of. Currently set to one folder, maybe being able to choose a folder coming later.
 path = "E:/Backups/Documents/Valokuvausprojekti/Omat/Renamechamber"
 
 def main():
+    #What will be the name of the new file. It will be printed as "*prefix*_*number 1 to infinity*.*jpg or nef*"
     prefix = input("Give your desired prefix for the files: ").capitalize()
     jpgcount = 1
     nefcount = 1
+    #Check if the file is a jpg or a nef file.
     for count, filename in enumerate(os.listdir(path)):
         if ".JPG" in filename or ".jpg" in filename:
             dst = f"{prefix}_{str(jpgcount)}.JPG"
@@ -14,13 +18,17 @@ def main():
             dst = f"{prefix}_{str(nefcount)}.NEF"
             nefcount += 1
         else:
+            #Failsafe
             print("something went wrong")
+            os.system("pause")
             exit()
 
+        #The actual renaming part. Src = the source path of the image and dst = the new path for the image, including the new name
         src = f"{path}/{filename}"
         dst = f"{path}/{dst}"
         os.rename(src, dst)
         
+    #After the renaming display a message and prevents the window from closing on itself.
     print("All files renamed!")
     os.system("pause")
             
